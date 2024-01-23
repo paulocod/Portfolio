28a94e1c8605265727354dc97c7445a3f1434bf2
Paulo Campos <paulo4-0@hotmail.com>
2024-01-23 04:04:52 -0300

    add env file por production

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
+  };
 
-export default nextConfig;
+  export default nextConfig;
