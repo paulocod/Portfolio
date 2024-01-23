cc1a5a94c9d33390eda4a398c3f718cc04cbce93
Paulo Campos <paulo4-0@hotmail.com>
2024-01-23 05:11:57 -0300

    export on next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
+  output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '',
 };
