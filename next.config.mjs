b3b076fd8dde3e0d35776fffff540547d15ab13b
Paulo Campos <paulo4-0@hotmail.com>
2024-01-23 04:19:36 -0300

    fixing the name of repositorie for deploy in gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '',
 };
 
 export default nextConfig;
