e8615cf8610bf28ac98243b2fad88df5c88c2d18
Paulo Campos <paulo4-0@hotmail.com>
2024-01-29 22:08:45 -0300

    configure the i18n nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
 const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '',
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
