1c9ffae553c0cad67c75047ec7e8aadde1806639
Paulo Campos <paulo4-0@hotmail.com>
2024-01-29 22:16:30 -0300

    making the main page configuration for i18n

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,10 +3,12 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import MovingIcons from "@/components/MovingItems";
 import Projects from "@/components/Projects";
-import { InfoText } from "@/data";
 import Image from 'next/image';
+import { useTranslations } from 'next-intl';
 
 export default function Home() {
+  const t = useTranslations('InfoText');
+
   const scrollToSection = (sectionId: string) => {
     const section = document.getElementById(sectionId);
 
@@ -20,24 +22,24 @@ export default function Home() {
       <section id="home" className="h-screen flex items-center justify-center mt-12 mb-20 relative z-10 overflow-hidden">
         <Image src="/Portfolio/imgs/grid-home.svg" alt="Projeto 1" layout="fill" className="rounded-l-lg object-cover" />
         <div className="h-1/2 text-center flex flex-col items-center">
-          <h1 className="text-6xl mb-4">{InfoText.HomeSectionText1}</h1>
-          <h1 className="text-6xl mb-4">{InfoText.HomeSectionText2} <strong className="text-orange-500">{InfoText.HomeSectionText3}</strong>{InfoText.HomeSectionText4}</h1>
-          <p className="text-5xl">{InfoText.HomeSectionText5}<strong className="text-orange-500">{InfoText.HomeSectionText6}</strong></p>
+          <h1 className="text-6xl mb-4">{t('HomeSectionText1')}</h1>
+          <h1 className="text-6xl mb-4">{t('HomeSectionText2')} <strong className="text-orange-500">{t('HomeSectionText3')}</strong>{t('HomeSectionText4')}</h1>
+          <p className="text-5xl">{t('HomeSectionText5')}<strong className="text-orange-500">{t('HomeSectionText6')}</strong></p>
         </div>
       </section>
       <div id="about" className="my-40">
-        <h1 className="text-4xl mb-4">{InfoText.AboutSectionText1}<strong className="text-orange-500">{InfoText.AboutSectionText2}</strong></h1>
-        <p className="text-3xl leading-10">{InfoText.AboutSectionText3}</p>
+        <h1 className="text-4xl mb-4">{t('AboutSectionText1')}<strong className="text-orange-500">{t('AboutSectionText2')}</strong></h1>
+        <p className="text-3xl leading-10">{t('AboutSectionText3')}</p>
       </div>
       <div className="my-40">
-        <h1 className="text-4xl mb-12">{InfoText.SkillsSectionText1}<strong className="text-orange-500"> & </strong>{InfoText.SkillsSectionText2}</h1>
+        <h1 className="text-4xl mb-12">{t('SkillsSectionText1')}<strong className="text-orange-500"> & </strong>{t('SkillsSectionText2')}</h1>
         <MovingIcons />
       </div>
       <Projects />
       <div id="contact" className="my-12 flex items-center justify-between px-4">
         <div>
-          <h1 className="text-4xl mb-2">{InfoText.FooterSectionText1}<strong className="text-orange-500">{InfoText.FooterSectionText2}</strong>?</h1>
-          <p className="text-xl mb-4 w-2/3">{InfoText.FooterSectionText3}</p>
+          <h1 className="text-4xl mb-2">{t('FooterSectionText1')}<strong className="text-orange-500">{t('FooterSectionText2')}</strong>?</h1>
+          <p className="text-xl mb-4 w-2/3">{t('FooterSectionText3')}</p>
         </div>
         <div className="relative w-96 h-96">
           <Image
