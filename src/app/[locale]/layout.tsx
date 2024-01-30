1406bcf37d2ec1e738d176e3b5d74f1316e59f5d
Paulo Campos <paulo4-0@hotmail.com>
2024-01-29 22:10:36 -0300

    configurate the locale path in main page

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { NextIntlClientProvider, useMessages } from 'next-intl'
+
+import { JetBrains_Mono } from 'next/font/google'
+
+const jetBrains = JetBrains_Mono({
+  subsets: ["latin"],
+  display: "swap"
+})
+
+export const metadata: Metadata = {
+  title: "Paulo Campos",
+  description: "Welcome to my portfolio",
+};
+
+type Props = {
+  children: React.ReactNode;
+  params: {
+    locale: "en" | "pt"
+  }
+}
+
+const RootLayout: React.FC<Props> = ({
+  children,
+  params: { locale }
+}) => {
+  const messages = useMessages()
+  return (
+    <html className={jetBrains.className} lang={locale}>
+      <body className="bg-zinc-900 text-zinc-50">
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout
