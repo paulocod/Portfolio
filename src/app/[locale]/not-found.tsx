5bed568182a554865d9e2a5a4f21e3635a1367b3
Paulo Campos <paulo4-0@hotmail.com>
2024-02-04 20:00:19 -0300

    fixing not found file

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,12 +1,8 @@
-import Head from "next/head"
 import Link from "next/link"
 
 const NotFound = () => {
     return (
         <>
-        <Head>
-            <title> 404 | Paulo Campos</title>
-        </Head>
             <div className="flex flex-col items-center justify-center h-screen bg-black text-white">
                 <h1 className="text-6xl font-bold mb-4">404</h1>
                 <p className="text-2xl mb-8">Página não encontrada</p>
