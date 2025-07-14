3749130a9cf90f47879783e09b983bb9ad631034
Paulo Campos <paulo4-0@outlook.com>
2025-07-14 00:56:55 -0300

    fix: error during build time vercel (#13)

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,6 @@ const nextConfig = {
   compress: true,
   poweredByHeader: false,
   generateEtags: true,
-  swcMinify: true,
   images: {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
